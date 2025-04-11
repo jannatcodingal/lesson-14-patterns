02c4daf5518d0e64eaef38657e23488a80911cf3
jannatcodingal <jannatrehman007@gmail.com>
2025-04-11 21:21:36 +0500

    diamond

--- diamond.py
+++ diamond.py
@@ -6,7 +6,7 @@ else:
 space=halfdiamrow-1
 for i in range(1,halfdiamrow+1):
     for j in range(1,space+1):
-        print(end="")
+      print(end="")
     space= space-1
     num=1
     for j in range(2*i-1):
